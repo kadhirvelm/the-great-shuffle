9b4c7c3c948fc5d34f16f920440ca67f2521930a
Kadhir Mani <kadhirvelm@gmail.com>
2023-11-19 11:51:08 -0800

    Implement network calls

--- packages/backend/src/app.controller.ts
+++ packages/backend/src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { AppService } from "./app.service";
+import { IBackendService, IService, RemoveString } from "@great-shuffle/api";
+
+type IServiceImplementation<Service extends IService> = {
+  [Key in keyof RemoveString<Service>]: (
+    payload: Service[Key]["payload"],
+  ) => Service[Key]["response"] | Promise<Service[Key]["response"]>;
+};
 
 @Controller()
-export class AppController {
+export class AppController implements IServiceImplementation<IBackendService> {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post()
+  helloWorld(@Body() _payload: IBackendService["helloWorld"]["payload"]) {
+    return this.appService.helloWorld();
   }
 }
