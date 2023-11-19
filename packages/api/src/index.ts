cac1b0004a7dd534cafd2090a7dc047239f9b5a9
Kadhir <kadhirvelm@gmail.com>
2023-11-19 11:53:41 -0800

    Implement network calls (#6)

---
+++ packages/api/src/index.ts
@@ -0,0 +1,31 @@
+export interface IService {
+  [key: string]: {
+    payload: any;
+    response: any;
+  };
+}
+
+export type RemoveString<Service extends IService> = {
+  [Key in keyof Service as string extends Key ? never : Key]: any;
+};
+
+type IServiceImplementation<Service> = {
+  [Key in keyof Service]: {
+    endpoint: string;
+  };
+};
+
+export interface IBackendService extends IService {
+  helloWorld: {
+    payload: {};
+    response: {
+      message: string;
+    };
+  };
+}
+
+export const backendService: IServiceImplementation<IBackendService> = {
+  helloWorld: {
+    endpoint: "/hello-world",
+  },
+};
---
+++ packages/frontend/src/lib/network.ts
@@ -0,0 +1,35 @@
+import {
+  IBackendService,
+  IService,
+  RemoveString,
+  backendService,
+} from "@great-shuffle/api";
+
+type NetworkImplementation<Service extends IService> = {
+  [Key in keyof RemoveString<Service>]: (
+    payload: Service[Key]["payload"],
+  ) => Promise<Service[Key]["response"]>;
+};
+
+export const networkCalls: NetworkImplementation<IBackendService> =
+  Object.fromEntries(
+    Object.entries(backendService).map(([endpoint, definition]) => {
+      return [
+        endpoint,
+        async (payload: any) => {
+          const response = await fetch(
+            `http://localhost:3000/${definition.endpoint}`,
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(payload),
+            },
+          );
+
+          return response.json();
+        },
+      ];
+    }),
+  ) as NetworkImplementation<IBackendService>;
