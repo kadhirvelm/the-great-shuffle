47c56eb0cbccd267baf4bb39419d5631a8a19230
Kadhir <kadhirvelm@gmail.com>
2023-12-27 21:01:26 -0800

    Refactor to isolate monsters service (#53)

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -3,10 +3,11 @@ import { ConfigModule } from "@nestjs/config";
 import { ServeStaticModule } from "@nestjs/serve-static";
 import { join } from "path";
 import { AppController } from "./app.controller";
-import { ImageManipulationService } from "./imageManipulation/imageManipulation.service";
-import { ChatGPTService } from "./chatgpt/chatgpt.service";
-import { GPTImageCleanUpService } from "./imageManipulation/gptImageCleanUp.service";
-import { RemoveBackgroundService } from "./removeBackground/removeBackground.service";
+import { ImageManipulationService } from "./helperServices/imageManipulation/imageManipulation.service";
+import { ChatGPTService } from "./helperServices/chatgpt/chatgpt.service";
+import { GPTImageCleanUpService } from "./helperServices/imageManipulation/gptImageCleanUp.service";
+import { RemoveBackgroundService } from "./helperServices/removeBackground/removeBackground.service";
+import { MonstersModule } from "./monsters/monsters.module";
 
 @Module({
   imports: [
@@ -14,8 +15,14 @@ import { RemoveBackgroundService } from "./removeBackground/removeBackground.ser
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, "..", "assets"),
     }),
+    MonstersModule,
   ],
   controllers: [AppController],
-  providers: [ImageManipulationService, ChatGPTService, GPTImageCleanUpService, RemoveBackgroundService],
+  providers: [
+    ImageManipulationService,
+    ChatGPTService,
+    GPTImageCleanUpService,
+    RemoveBackgroundService,
+  ],
 })
 export class AppModule {}
